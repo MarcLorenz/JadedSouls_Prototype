ad00baccaf54a59e79199aac7a268af8f67d71d1
MarcLorenz <mrod@ucdavis.edu>
2014-10-23 16:59:24 -0700

    Jaded Souls
    Prototype

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerJump : MonoBehaviour {
+
+	public bool isGrounded = true;
+	public int jumpPower = 1;
+
+	// Use this for initialization
+	void Start(){
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ((Input.GetButtonDown("Jump") || Input.GetKey("space")) && isGrounded)
+		{
+			Jump();
+		}
+	}
+
+	void Jump(){
+		Debug.Log(isGrounded+"1");
+			rigidbody.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Force);
+		isGrounded = false;
+		}
+
+	void FixedUpdate(){
+
+		isGrounded = Physics.Raycast(transform.position, -Vector3.up, 0.5f);
+
+		Debug.Log (isGrounded);
+		}
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public float speed = 6f;
+
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidbody;
+	int floorMask;
+
+	void Awake()
+	{
+		floorMask = LayerMask.GetMask ("Floor");
+		anim = GetComponent <Animator> ();
+		playerRigidbody = GetComponent <Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h);
+		Animating (h);
+	}
+
+	void Move (float h)
+	{
+		movement.Set (h, 0f, 0f);
+
+		movement = movement * speed * Time.deltaTime;
+
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+
+	void Animating (float h)
+	{
+		bool running = h != 0f;
+		anim.SetBool ("IsRunning", running);
+	}
+}
