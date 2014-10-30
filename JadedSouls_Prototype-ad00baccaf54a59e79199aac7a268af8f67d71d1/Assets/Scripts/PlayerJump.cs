6d512571b67d4952c4d176dae3ed380c722059fb
MarcLorenz <mrod@ucdavis.edu>
2014-10-30 04:01:15 -0700

    Jump and Rotate - Fixed
    Character now properly jumps and rotates

---
+++ JadedSouls_Prototype-ad00baccaf54a59e79199aac7a268af8f67d71d1/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerJump : MonoBehaviour {
+
+	public bool isGrounded = true;
+	public int jumpPower = 1;
+
+	// Use this for initialization
+	void Start(){
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ((Input.GetButtonDown("Jump") || Input.GetKey("space")) && isGrounded)
+		{
+			Jump();
+		}
+	}
+
+	void Jump(){
+					rigidbody.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Force);
+		isGrounded = false;
+		}
+
+	void FixedUpdate(){
+
+		isGrounded = Physics.Raycast(transform.position, -Vector3.up, 0.1f);
+
+		}
+
+}
---
+++ JadedSouls_Prototype-ad00baccaf54a59e79199aac7a268af8f67d71d1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public Vector3 Right;
+	public Vector3 Left;
+	public float speed = 6f;
+	public Vector3 newPos;
+
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidbody;
+	int floorMask;
+
+	void Awake()
+	{
+		floorMask = LayerMask.GetMask ("Floor");
+		anim = GetComponent <Animator> ();
+		playerRigidbody = GetComponent <Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h);
+		Animating (h);
+	}
+
+	void Move (float h)
+	{
+		movement.Set (h, 0f, 0f);
+
+		movement = movement * speed * Time.deltaTime;
+
+		playerRigidbody.MovePosition (transform.position + movement);
+
+		Right = new Vector3 (100000 , 0, 0);
+		Left = new Vector3 (-100000, 0, 0);
+
+		if ((transform.position != Right) && Input.GetKey("d")){
+			transform.LookAt(Right);
+		}
+		if ((transform.position != Left) && Input.GetKey("a")){
+			transform.LookAt(Left);
+		}
+	}
+	
+	void Animating (float h)
+	{
+		bool running = h != 0f;
+		anim.SetBool ("IsRunning", running);
+	}
+}
