c8d9dc6a4ce688d4f8ae912ec8ba939177e423db
Eric La <eric_l248@yahoo.com>
2014-11-07 21:02:31 -0800

    added doublejump, air movement

--- JadedSouls_Prototype-ad00baccaf54a59e79199aac7a268af8f67d71d1/Assets/Scripts/PlayerJump.cs
+++ JadedSouls_Prototype-ad00baccaf54a59e79199aac7a268af8f67d71d1/Assets/Scripts/PlayerJump.cs
@@ -4,7 +4,10 @@ using System.Collections;
 public class PlayerJump : MonoBehaviour {
 
 	public bool isGrounded = true;
-	public int jumpPower = 1;
+	public int jumpPower = 20000;
+	bool pressed = false;
+	public int jumps = 2;
+	Vector3 cur;
 
 	// Use this for initialization
 	void Start(){
@@ -13,20 +16,34 @@ public class PlayerJump : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if ((Input.GetButtonDown("Jump") || Input.GetKey("space")) && isGrounded)
+		if ((Input.GetButtonDown("Jump") || Input.GetKey("space")) && (isGrounded ||  jumps > 0) && !pressed)
 		{
+			--jumps;
+			pressed = true;
 			Jump();
 		}
+
+		if((Input.GetButtonUp("Jump") || Input.GetKeyUp("space"))){
+			pressed = false;
+		} 
 	}
 
 	void Jump(){
-					rigidbody.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Force);
+		rigidbody.useGravity = false;//turn off gravity, set v vel to 0, turn back on
+		cur = rigidbody.velocity;
+		cur.y = 0f;
+		rigidbody.velocity = cur;//set current vertical vel to 0
+		rigidbody.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Force);
+		rigidbody.useGravity = true;
 		isGrounded = false;
 		}
 
 	void FixedUpdate(){
 
 		isGrounded = Physics.Raycast(transform.position, -Vector3.up, 0.1f);
+		
+		if(isGrounded && !pressed)
+			jumps = 2;
 
 		}
 
--- JadedSouls_Prototype-ad00baccaf54a59e79199aac7a268af8f67d71d1/Assets/Scripts/PlayerMovement.cs
+++ JadedSouls_Prototype-ad00baccaf54a59e79199aac7a268af8f67d71d1/Assets/Scripts/PlayerMovement.cs
@@ -1,56 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class PlayerMovement : MonoBehaviour 
+public class PlayerMovement : MonoBehaviour
 {
 	public Vector3 Right;
 	public Vector3 Left;
 	public float speed = 6f;
 	public Vector3 newPos;
+	PlayerJump jump;
+	public float jump_factor = 3f/4f;
+	public bool running = false;
 
 	Vector3 movement;
 	Animator anim;
 	Rigidbody playerRigidbody;
 	int floorMask;
-
 	void Awake()
 	{
 		floorMask = LayerMask.GetMask ("Floor");
 		anim = GetComponent <Animator> ();
 		playerRigidbody = GetComponent <Rigidbody> ();
-	}
+		jump = playerRigidbody.GetComponent <PlayerJump>();		
 
+		Right = new Vector3 (100000 , 0, 0);
+		Left = new Vector3 (-100000, 0, 0);
+	}
 	void FixedUpdate()
 	{
 		float h = Input.GetAxisRaw ("Horizontal");
 		float v = Input.GetAxisRaw ("Vertical");
-
 		Move (h);
 		Animating (h);
 	}
-
 	void Move (float h)
 	{
-		movement.Set (h, 0f, 0f);
-
+		if(jump.isGrounded){
+			movement.Set (h, 0f, 0f);
+		}
+		else {
+			movement.Set (h * jump_factor, 0f, 0f);
+		}
 		movement = movement * speed * Time.deltaTime;
-
 		playerRigidbody.MovePosition (transform.position + movement);
 
-		Right = new Vector3 (100000 , 0, 0);
-		Left = new Vector3 (-100000, 0, 0);
-
-		if ((transform.position != Right) && Input.GetKey("d")){
+		if ((transform.position != Right) && h > 0 ){
 			transform.LookAt(Right);
 		}
-		if ((transform.position != Left) && Input.GetKey("a")){
+		if ((transform.position != Left) && h < 0){
 			transform.LookAt(Left);
 		}
 	}
-	
 	void Animating (float h)
 	{
-		bool running = h != 0f;
+		if(jump.isGrounded){
+			running = h != 0f;
+		}
+		else {
+			running = false;
+		}
 		anim.SetBool ("IsRunning", running);
 	}
 }
